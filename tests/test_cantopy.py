c0b551fe7bab8ef493e5e11890804f509e7c5183
RobbeRDG <robbedegroeve@gmail.com>
2023-12-17 14:28:03 +0000

    Finished general api testing

--- tests/test_cantopy.py
+++ tests/test_cantopy.py
@@ -5,17 +5,10 @@ cantopy = CantoPy()
 
 def test_query_singlepage():
     # Send a simple query
-    query = Query(name="Common Blackbird", q="A")
+    query = Query(name="common blackbird", q="A")
     results = cantopy.send_query(query)
 
     # See if the QueryResult object contain the requested information
     assert len(results) == 1  # We only requested a single page
     assert results[0].recordings[0].en == "Common Blackbird"
     assert results[0].recordings[0].q == "A"
-
-if __name__ == "__main__":
-    # Send a simple query
-    query = Query(name="common blackbird", q="A")
-    stringtest = query.to_string()
-    results = cantopy.send_query(query)
-    print()
