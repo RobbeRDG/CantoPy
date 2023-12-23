8f55b39db8ad2b975267cb6958578f4428801a90
RobbeRDG <robbedegroeve@gmail.com>
2023-12-23 11:54:54 +0000

    Refactor ResultPage test

--- tests/components/test_resultpage.py
+++ tests/components/test_resultpage.py
@@ -10,14 +10,12 @@ def test_resultpage_init():
         "tests/test_resources/xenocanto_query_response.json", "r", encoding="utf-8"
     ) as file:
         query_response = json.load(file)
-    query_result = ResultPage(query_response)
+    result_page = ResultPage(query_response)
 
-    assert query_result.available_num_recordings == 67810
-    assert query_result.available_num_species == 1675
-    assert query_result.page == 5
-    assert query_result.available_num_pages == 136
+    # Test page attribute
+    assert result_page.page == 5
 
     # Just check if recording is also set,
     # but more detailed recording evaluation is in the Recording test section
-    assert len(query_result.recordings) == 3
-    assert query_result.recordings[0].id == 581412
+    assert len(result_page.recordings) == 3
+    assert result_page.recordings[0].id == 581412
