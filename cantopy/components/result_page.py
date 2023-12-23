14e3381295e273bd701b0e254b3876e7ba6f423a
RobbeRDG <robbedegroeve@gmail.com>
2023-12-23 11:54:26 +0000

    Refactor ResultPage class by removing unused attributes (global query attributes)

--- cantopy/components/result_page.py
+++ cantopy/components/result_page.py
@@ -7,14 +7,8 @@ class ResultPage:
 
     Attributes
     ----------
-    available_num_recordings : int
-        The total number of recordings available for this query, independent of page limit.
-    available_num_species : int
-        The total number of species available for this query, independent of page limit.
     page : int
         The page number of the results page that is being displayed.
-    available_num_pages : int
-        The total number of pages available for this query.
     recordings : List[Recording]
         An array of recording objects containing detailed information about each recording.
 
@@ -29,10 +23,7 @@ class ResultPage:
             The response from the XenoCanto API.
         """
 
-        self.available_num_recordings = int(single_page_query_response["numRecordings"])
-        self.available_num_species = int(single_page_query_response["numSpecies"])
         self.page = int(single_page_query_response["page"])
-        self.available_num_pages = int(single_page_query_response["numPages"])
         self.recordings: List[Recording] = []
         for query_response_recording in single_page_query_response.get("recordings", []):
             self.recordings.append(Recording(query_response_recording))
