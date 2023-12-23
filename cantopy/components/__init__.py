72c30da615a82c336cf836518cb8629172cb09c4
RobbeRDG <robbedegroeve@gmail.com>
2023-12-23 11:53:47 +0000

    Add QueryResult import

--- cantopy/components/__init__.py
+++ cantopy/components/__init__.py
@@ -1,3 +1,4 @@
 from cantopy.components.query import Query
 from components.result_page import ResultPage
 from cantopy.components.recording import Recording
+from cantopy.components.query_result import QueryResult
