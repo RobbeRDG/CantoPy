f43e2210a231fdba360c2287a6734ad75d6eaa22
RobbeRDG <robbedegroeve@gmail.com>
2023-12-25 20:28:15 +0000

    Refactor test_query_result_initialization function with parametrize

--- tests/components/test_queryresult.py
+++ tests/components/test_queryresult.py
@@ -1,48 +1,38 @@
 from cantopy import ResultPage
-from components.query_result import QueryResult
+import pytest
 
 
-def test_query_result_single_page(
-    example_single_page_queryresult: QueryResult, example_result_page: ResultPage
+@pytest.mark.parametrize(
+    "example_queryresult_fixture_name, expected_num_pages",
+    [
+        ("example_single_page_queryresult", 1),
+        ("example_three_page_queryresult", 3),
+    ],
+)
+def test_query_result_initialization(
+    example_queryresult_fixture_name: str,
+    expected_num_pages: int,
+    example_result_page: ResultPage,
+    request: pytest.FixtureRequest,
 ):
-    """Test the initialisation of a QueryResult object containing a single page.
+    """Test the initialization of a QueryResult object.
 
     Parameters
     ----------
-    example_single_page_queryresult : QueryResult
-        A three-page QueryResult object based on the example XenoCanto API query response.
+    example_queryresult : QueryResult
+        A QueryResult object based on the example XenoCanto API query response.
     example_result_page : ResultPage
         The ResultPage object created from the example XenoCanto API query response.
+    expected_num_pages : int
+        The expected number of result pages.
     """
+    example_queryresult = request.getfixturevalue(example_queryresult_fixture_name)
 
-    # Check attirbutes
-    assert example_single_page_queryresult.available_num_recordings == 67810
-    assert example_single_page_queryresult.available_num_species == 1675
-    assert example_single_page_queryresult.available_num_pages == 136
+    # Check attributes
+    assert example_queryresult.available_num_recordings == 67810
+    assert example_queryresult.available_num_species == 1675
+    assert example_queryresult.available_num_pages == 136
 
     # Check stored result pages
-    assert len(example_single_page_queryresult.result_pages) == 1
-    assert example_single_page_queryresult.result_pages[0] == example_result_page
-
-
-def test_query_result_multi_page(
-    example_three_page_queryresult: QueryResult, example_result_page: ResultPage
-):
-    """Test the initialisation of a QueryResult object containing multiple pages (3 pages).
-
-    Parameters
-    ----------
-    example_single_page_queryresult : QueryResult
-        A three-page QueryResult object based on the example XenoCanto API query response.
-    example_result_page : ResultPage
-        The ResultPage object created from the example XenoCanto API query response.
-    """
-
-    # Check attirbutes
-    assert example_three_page_queryresult.available_num_recordings == 67810
-    assert example_three_page_queryresult.available_num_species == 1675
-    assert example_three_page_queryresult.available_num_pages == 136
-
-    # Check stored result pages
-    assert len(example_three_page_queryresult.result_pages) == 3
-    assert example_three_page_queryresult.result_pages[0] == example_result_page
+    assert len(example_queryresult.result_pages) == expected_num_pages
+    assert example_queryresult.result_pages[0] == example_result_page
